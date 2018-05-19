cec096aee1194c3b3df4c600ad64a6067b0ee96f
abdullahkady <abdullahkady@gmail.com>
2018-05-19 13:38:49 +0200

    Add process counter method

--- src/lottery.c
+++ src/lottery.c
@@ -1,3 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Given the file name, it returns how many
+// processes exist in the file (lines -2)
+int numOfProcesses(char *fileName)
+{
+  FILE *file = fopen(fileName, "r");
+  if (!file)
+  {
+    return 0;
+  }
+  int result = 0;
+  char buf[200];
+  while (fgets(buf, sizeof(buf), file))
+  {
+    result++;
+  }
+  fclose(file);
+  return result - 2;
+}
+
 main()
 {
   return 0;
