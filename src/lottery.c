de37d46843d9b8b3410a00c2f9de19b093d49475
abdullahkady <abdullahkady@gmail.com>
2018-05-25 09:18:23 +0200

    Read the file properly into variables

--- src/lottery.c
+++ src/lottery.c
@@ -44,7 +44,59 @@ int randomInclusive(int min, int max)
   return min + rand() / (RAND_MAX / (max - min + 1) + 1);
 }
 
-main()
+int main()
 {
-  return 0;
+  time_t start, end, tmpClock;
+  char inputFileName[256];
+  char outputFileName[256];
+  printf("Enter input file name: ");
+  scanf("%s", inputFileName);
+  printf("Enter Output file name: ");
+  scanf("%s", outputFileName);
+  int processID = 0, totalNumOfProcesses, timeLine, remain, isFinished = 0, quantumLength;
+  totalNumOfProcesses = numOfProcesses(inputFileName);
+  int waitTime = 0;
+  int turnAround = 0;
+  int arrivalTime[totalNumOfProcesses], cpuBurst[totalNumOfProcesses], remainingTime[totalNumOfProcesses], lotteryTickets[totalNumOfProcesses];
+  FILE *file = fopen(inputFileName, "r");
+  if (!file)
+  {
+    printf("FILE DOESN'T EXIST!!\n");
+    return 0;
+  }
+  char quantum_string[256];
+  fgets(quantum_string, sizeof(quantum_string), file);
+  quantumLength = atoi(quantum_string); //Converting the quantum into an int
+  remain = totalNumOfProcesses;
+
+  char total_tickets_string[256];
+  fgets(total_tickets_string, sizeof(total_tickets_string), file);
+  int totalTickets = atoi(total_tickets_string);
+
+  char tmpProcess[256];
+  while (fgets(tmpProcess, sizeof(tmpProcess), file))
+  {
+    //Converting each element into an int, and storing them in the appropriate corresponding array cells.
+    char *dup = strdup(tmpProcess);
+    int currentArrivalTime = atoi(getElement(dup, 2));
+    dup = strdup(tmpProcess);
+    int currentCpuBurst = atoi(getElement(dup, 3));
+    dup = strdup(tmpProcess);
+    int currentTickets = atoi(getElement(dup, 4));
+
+    arrivalTime[processID] = currentArrivalTime;
+    cpuBurst[processID] = currentCpuBurst;
+    remainingTime[processID] = cpuBurst[processID];
+    if (processID == 0)
+    {
+      lotteryTickets[processID] = currentTickets;
+    }
+    else
+    {
+      lotteryTickets[processID] = lotteryTickets[processID - 1] + currentTickets;
+    }
+    processID++;
+  }
+  fclose(file);
+  //LOADING ALL VALUES DONE
 }
